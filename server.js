c18899370207c4793b5f186740ce9cd29b375f63
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-07-08 22:46:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,28 +7,27 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.sendFile(INDEX, {
+        root: __dirname
+    }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-const { Server } = require('ws');
+const {Server} = require('ws');
 
-const wss = new Server({ server });
+const wss = new Server({server});
 
 var connections = [];
 wss.on('connection', (ws) => {
-	console.log('Client connected');
-  	ws.on('message', function(message) {
-		ws.send('ECHO' + message);
-	});
-	ws.on('close', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    ws.on('message', function(message) {
+        wss.clients.forEach((client) => {
+            client.send(message);
+        });
+    });
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 5000);
 
 // const io = socketIO(server);
 
